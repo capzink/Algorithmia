fdc7b01150d444d2f4a7ac26a189fde2d5a47078
Carlos <capz2003@gmail.com>
2021-11-19 07:59:26 -0500

    2006

--- 2006.js
+++ 2006.js
@@ -1,1 +1,17 @@
 console.clear()
+
+const countKDifference = (nums, k) => {
+    let counter =0
+  for (let i in nums) {
+      for (let j in nums){
+          if(Math.abs(nums[i] - nums[j]=== k)) {
+              counter ++
+          }
+          
+        }
+    }
+    return counter
+}
+console.log(countKDifference([3,2,1,5,4],2))
+
+//[3,2,1,5,4], k = 2
